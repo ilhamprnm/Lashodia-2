2ade192acf8db5c95b600f745c8f0be21e170ba3
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-07-16 20:17:41 +0700

    Set up react with vite and tailwindcss, make navbar component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Login' element={<Login />}/>
+        <Route path='/Signup' element={<Signup />}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
