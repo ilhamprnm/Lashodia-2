c0214a2e4924dc2f80bcfdf672fdab20d03c8151
Muhammad Ilham Purnama <ilhampurnama78@gmail.com>
2024-08-27 10:51:33 +0700

    Adjust Footer text color

--- src/components/Home/Copyright.jsx
+++ src/components/Home/Copyright.jsx
@@ -4,7 +4,7 @@ const Copyright = () => {
   return (
     <div className='bg-black text-center py-3 border-t border-t-gray-50'>
       
-      <p className='text-white text-sm'>Designed By @mdrimel15 on Figma</p>
+      <p className='text-gray-400 text-sm'>Designed By @mdrimel15 on Figma</p>
     </div>
   )
 }
